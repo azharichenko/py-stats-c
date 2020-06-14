1a8cfbb90d4448f64c35ecf823ff2712e22ea51a
Alex D. Zharichenko <azharichenko@gmail.com>
2020-06-13 23:40:50 -0400

    Started conversion to boost python

--- build_n_run.sh
+++ build_n_run.sh
@@ -1,5 +1,9 @@
 #/bin/bash
-rm -rf *.so
-rm -rf build/
-python3 setup.py build_ext --inplace
-pipenv run python3 -m pytest test.py
+# rm -rf *.so
+# rm -rf build/
+# python3 setup.py build_ext --inplace
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=DEBUG .. && make 
+python3 test.py
+# pipenv run python3 -m pytest test.py
